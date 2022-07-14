acff53fb5dcdff08a2bced66f35e1537626ced27
Rafael de Jesus Silva Monteiro <rafael.rjsm@gmail.com>
2022-07-14 15:26:59 -0300

    Update curvasn.py

--- ORCAMENTO/curvasn.py
+++ ORCAMENTO/curvasn.py
@@ -5,9 +5,9 @@ def cvsn(n):
     '''
     Gera uma curva S de desembolso considerando uma S padrão normal
     '''
-    x = np.arange(n)
-    ynac = [norm.cdf((t+1)/n,0.5,0.2) for t in x]
-    y = [ynac[0]] + [ynac[t+1]-ynac[t] for t in x[:-1]]
+    ynac = np.array([norm.cdf((t+1)/n,0.5,0.2) for t in range(n)])
+    y = ynac.copy()
+    y[1:] -= ynac[:-1].copy()
     # Garantindo que o acumulado total seja 100%
     y = y/sum(y)
     ynac = np.cumsum(y)
